8b7f8f085f19c8f0f2731dabc5deaccce8c5e0e5
mohammedterfa <mohammedterfa@gmail.com>
2022-04-05 21:26:20 +0200

    single product function in the controller was set

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -12,6 +12,11 @@ class ProjectController extends Controller
         return view('index');
     }
 
+    public function single_product(Request $request, $id){
+        $product_array = DB::table('products')->where('id',$id)->get();
+        return view('single_product',['product_array'=>$product_array]);
+    }
+
     public function products(){
         $products = DB::table('products')->get();
         return view('products', ['products'=>$products]);
